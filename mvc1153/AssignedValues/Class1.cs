a771afb384dd2d750d7798680821b627f1894ef4
ShahSavier <alif.shahkimi99@gmail.com>
2021-10-25 14:48:41 +0800

    Create Database

---
+++ mvc1153/AssignedValues/Class1.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvc1153.AssignedValues
+{
+    /* Namas asal PosLaju adaalah Class1*/
+    public class PosLaju
+    {
+        public string[] weightCategories =
+        {
+            /* Just using for label not calculation*/
+            "< 0.50 kg", "< 0.75 kg", "< 1.00 kg", "< 1.25 kg",
+            "< 1.50 kg", "< 1.75kg", "< 2.00 kg", "< 2.50 kg",
+            "< 3.00 kg"
+        };
+
+        public string[] zones =
+        {
+            "West Malaysia", "Sarawak" ,"Sabah"
+        };
+
+        public double[,] rates = /*2 dimensional array*/
+        {
+            { 6100 , 8.50, 9.00 },
+            { 7.00, 10.50, 12.00 },
+            { 8.50, 12.50, 14.50 },
+            { 10.00, 14.50, 17.00 },
+            { 11.00, 16.50, 20.00 },
+            { 12.50, 18.50, 22.50 },
+            { 14.00, 20.50, 25.00 },
+            { 21.00, 34.50, 41.00 },
+            { 24.00, 39.00, 46.00 }
+        };
+    }
+    // Jagged Array 1 Dimensional
+    public class SoftwareHouse
+    {
+        //one dimensional array string & jagged array
+        public string[] employees = { "Sarah", "Hafiz", "Amin" };
+
+        //jagged array part
+        public string[][] skills =
+        {
+            new string[]{"C#", "VB", "Java", "PHP"}, //Skills set for sarah
+            new string[]{"Python", "R"}, // Skills for Hafiz
+            new string[]{"React", "Ionic", "Flutter"} // skills for Amin
+        };
+    }
+
+    //Jagged Array 2 Dimensional
+    public class CourseGrade
+    {
+        public string[] students = { "Sarah", "Hafiz", "Amin" };
+
+        public string[][,] courses =
+        {
+            new string[,]{ //represent marks for Sarah "First row"
+                {"C#", "A"/*(index 0)*/}, { "VB", "B+"/*(index 1)*/},
+                {"Java","B"/*(index 2)*/}, {"PHP", "A-"/*(index 3)*/}},
+
+            new string[,] { //represent marks for Hafiz "Second row"
+                {"Python", "B"/*(Index 0)*/},{"R","A-"/*(Index 1)*/}},
+
+            new string[,]
+            {
+                {"React","C+"/*(Index 0)*/},{"Ionic","B"/*(Index 1)*/},
+                {"Flutter","A"/*(Index 2)*/}
+            }
+        };
+    }
+}
---
+++ mvc1153/Controllers/PosLajuParcelController.cs
@@ -0,0 +1,254 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using mvc1153.Models;
+
+namespace mvc1153.Controllers
+{
+    public class PosLajuParcelController : Controller
+    {
+        //read information from startup.cs
+        //using Microsoft.Extensions.Configuration;
+        private readonly IConfiguration configuration;
+
+        // constructr to postLaju Parcel
+        public PosLajuParcelController(IConfiguration config)
+        {
+            this.configuration = config;
+        }
+
+        IList<PosLajuParcel> GetDbList()
+        {
+            IList<PosLajuParcel> dbList = new List<PosLajuParcel>();
+            SqlConnection conn = new SqlConnection(configuration.GetConnectionString("ParcelConnStr"));
+
+            string sql = @"SELECT * FROM PosLajuParcel";
+
+            SqlCommand cmd = new SqlCommand(sql, conn);
+
+            try
+            {
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    dbList.Add(new PosLajuParcel()
+                    {
+                        ViewId = reader.GetString(0),
+                        ViewDateTime = reader.GetDateTime(1),
+                        SenderName = reader.GetString(2),
+                        SenderAddress = reader.GetString(3),
+                        SenderPhone = reader.GetString(4),
+                        SenderEmail = reader.GetString(5),
+                        ReceiverName = reader.GetString(6),
+                        ReceiverAddress = reader.GetString(7),
+                        ReceiverPhone = reader.GetString(8),
+                        ReceiverEmail = reader.GetString(9),
+                        IndexWeight = reader.GetInt32(10),
+                        IndexZone = reader.GetInt32(11),
+                        Amount = reader.GetDouble(12)
+                    });
+                }
+            }
+            catch
+            {
+                RedirectToAction("Error");
+            }
+            finally 
+            {
+                conn.Close();
+            }
+            return dbList;
+        }
+        public IActionResult Index()
+        {
+            IList<PosLajuParcel> dbList = GetDbList();
+
+            //return dblist here
+            return View(dbList);
+        }
+        public IActionResult Error() 
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ParcelDelivery()
+        {
+            //must sign value, if null will cause error
+            PosLajuParcel parcel = new PosLajuParcel();
+            parcel.IndexWeight = parcel.IndexZone = -1;
+            return View(parcel); //pass parcell to view
+
+            /** 
+             a few method
+            First method
+            -> parcel.IndexWeight = -1;
+               parcel.IndexZone = -1;
+
+            Second method 
+            -> parcel.IndexWeight = parcel.IndexZone = -1;
+
+            use this method to assign select zone & select weight
+             */
+        }
+
+        [HttpPost]
+        public IActionResult ParcelDelivery(PosLajuParcel parcel)
+        {
+            if (ModelState.IsValid) 
+            {
+                SqlConnection conn = new SqlConnection(configuration.GetConnectionString("ParcelConnStr"));
+                //ParcelConnStr take from appsettings.json
+                SqlCommand cmd = new SqlCommand("spInsertParcel", conn);
+                //spInsertParcel take from stored procedure
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@parcelid", parcel.ParcelId);
+                cmd.Parameters.AddWithValue("@parceldatetime", parcel.ParcelDateTime);
+                cmd.Parameters.AddWithValue("@sendername", parcel.SenderName);
+                cmd.Parameters.AddWithValue("@senderaddress",parcel.SenderAddress);
+                cmd.Parameters.AddWithValue("@senderphone",parcel.SenderPhone);
+
+                //email sender part
+                if (parcel.SenderEmail != null)
+                    cmd.Parameters.AddWithValue("@senderemail", parcel.SenderEmail);
+                else
+                    cmd.Parameters.AddWithValue("@senderemail", "");
+
+                cmd.Parameters.AddWithValue("@receivername", parcel.ReceiverName);
+                cmd.Parameters.AddWithValue("@receiveraddress", parcel.ReceiverAddress);
+                cmd.Parameters.AddWithValue("@receiverphone", parcel.ReceiverPhone);
+
+                //email receiver part
+                if (parcel.ReceiverEmail != null)
+                    cmd.Parameters.AddWithValue("@receiveremail", parcel.ReceiverEmail);
+                else
+                    cmd.Parameters.AddWithValue("@receiveremail", "");
+
+                cmd.Parameters.AddWithValue("@indexweight", parcel.IndexWeight);
+                cmd.Parameters.AddWithValue("@indexzone", parcel.IndexZone);
+                cmd.Parameters.AddWithValue("@amount", parcel.Amount);
+
+                try
+                {
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+                catch 
+                {
+                    return View(parcel);
+                }
+
+                return View("ParcelDeliveryInvoice", parcel);
+            }
+                
+            else
+                return View(parcel);
+            /**
+             Before validation => return View("ParcelDeliveryInvoice", parcel);
+             After validation =>
+                    if (ModelState.IsValid)
+                    return View("ParcelDeliveryInvoice", parcel);
+                else
+                    return View(parcel);
+             */
+        }
+        public IActionResult Details(string id)
+        {
+            IList<PosLajuParcel> dbList = GetDbList();
+            var result = dbList.First(x => x.ViewId == id);
+
+            return View(result);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(string id)
+        {
+            IList<PosLajuParcel> dbList = GetDbList();
+            var result = dbList.First(x => x.ViewId == id);
+
+            return View(result);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(string id, PosLajuParcel parcel)
+        {
+            SqlConnection conn = new SqlConnection(configuration.GetConnectionString("ParcelConnStr"));
+            SqlCommand cmd = new SqlCommand("spUpdateParcel", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.Parameters.AddWithValue("@receivername", parcel.ReceiverName);
+            cmd.Parameters.AddWithValue("@receiveraddress", parcel.ReceiverAddress);
+            cmd.Parameters.AddWithValue("@receiverphone", parcel.ReceiverPhone);
+            if (parcel.ReceiverEmail != null)
+                cmd.Parameters.AddWithValue("@receiveremail", parcel.ReceiverEmail);
+            else
+                cmd.Parameters.AddWithValue("@receiveremail", "");
+
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch
+            {
+                RedirectToAction("Error");
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(string id)
+        {
+            IList<PosLajuParcel> dbList = GetDbList();
+            var result = dbList.First(x => x.ViewId == id);
+
+            return View(result);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult ConfirmDelete(string id)
+        {
+            SqlConnection conn = new SqlConnection(configuration.GetConnectionString("ParcelConnStr"));
+            SqlCommand cmd = new SqlCommand("spDeleteParcel", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@id", id);
+
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public IActionResult SearchIndex(string searchString = "")
+        {
+            IList<PosLajuParcel> dbList = GetDbList();
+            var result = dbList.Where(x => x.ViewId.ToLower().Contains(searchString.ToLower()) ||
+            x.SenderName.ToLower().Contains(searchString.ToLower())).OrderBy(x => x.SenderName).ThenByDescending(x => x.ViewDateTime);
+            return View("Index", result);
+        }
+    }
+}
